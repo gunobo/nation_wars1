8c002451f59a23916ad2c03131f7ad8a3fa28d96
gunobo <113815207+gunobo@users.noreply.github.com>
2024-12-09 13:14:20 +0900

    Update NationWarPlugin.java

--- src/main/java/com/SubToGgunobo/nationwar/NationWarPlugin.java
+++ src/main/java/com/SubToGgunobo/nationwar/NationWarPlugin.java
@@ -5,12 +5,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class NationWarPlugin extends JavaPlugin {
     @Override
 public void onEnable() {
+    nationManager = new NationManager();
+    // 루트 디렉토리에서 nations.txt를 읽어오도록 경로 설정
+    nationManager.loadNationsFromFile(getDataFolder() + "/../nations.txt"); 
     getLogger().info("국가전쟁 플러그인이 활성화되었습니다.");
     getServer().getPluginManager().registerEvents(new BrewingStandListener(), this);
     getServer().getPluginManager().registerEvents(new EnchantmentListener(), this);
     getServer().getPluginManager().registerEvents(new PersonalChestListener(), this);
     getServer().getPluginManager().registerEvents(new RTPListener(), this);
     getServer().getPluginManager().registerEvents(new DeathBanListener(), this);
+    getServer().getPluginCommand("국가").setExecutor(new NationCommandListener(nationManager));
 }
 
     @Override
