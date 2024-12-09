767321bc1896b07e01b483b02d0e949e095770d7
gunobo <113815207+gunobo@users.noreply.github.com>
2024-12-09 12:57:07 +0900

    Update NationWarPlugin.java

--- src/main/java/com/SubToGgunobo/nationwar/NationWarPlugin.java
+++ src/main/java/com/SubToGgunobo/nationwar/NationWarPlugin.java
@@ -4,10 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class NationWarPlugin extends JavaPlugin {
     @Override
-    public void onEnable() {
-        getLogger().info("국가전쟁 플러그인이 활성화되었습니다.");
-        // 명령어 및 이벤트 리스너 등록
-    }
+public void onEnable() {
+    getLogger().info("국가전쟁 플러그인이 활성화되었습니다.");
+    getServer().getPluginManager().registerEvents(new BrewingStandListener(), this);
+}
 
     @Override
     public void onDisable() {
