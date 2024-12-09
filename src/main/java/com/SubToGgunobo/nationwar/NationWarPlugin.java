052945beabb7bf50b157c96229c4fb18211767cb
gunobo <113815207+gunobo@users.noreply.github.com>
2024-12-09 12:53:50 +0900

    Update NationWarPlugin.java

--- src/main/java/com/SubToGgunobo/nationwar/NationWarPlugin.java
+++ src/main/java/com/SubToGgunobo/nationwar/NationWarPlugin.java
@@ -1,1 +1,16 @@
+package com.yourname.nationwar;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NationWarPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("국가전쟁 플러그인이 활성화되었습니다.");
+        // 명령어 및 이벤트 리스너 등록
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("국가전쟁 플러그인이 비활성화되었습니다.");
+    }
+}
