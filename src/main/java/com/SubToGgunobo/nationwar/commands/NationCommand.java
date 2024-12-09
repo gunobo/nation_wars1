2d311c09ef5bc4b33c5754c1d8fc22f35db9c89b
gunobo <113815207+gunobo@users.noreply.github.com>
2024-12-09 12:54:04 +0900

    Update NationCommand.java

--- src/main/java/com/SubToGgunobo/nationwar/commands/NationCommand.java
+++ src/main/java/com/SubToGgunobo/nationwar/commands/NationCommand.java
@@ -1,1 +1,13 @@
+package com.yourname.nationwar.commands;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class NationCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // 명령어 처리 로직
+        return true;
+    }
+}
