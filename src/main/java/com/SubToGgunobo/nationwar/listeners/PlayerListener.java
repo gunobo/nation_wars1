29fd4019931ce37da9620bd667cbf54e9734a29f
gunobo <113815207+gunobo@users.noreply.github.com>
2024-12-13 20:02:18 +0900

    Update PlayerListener.java

--- src/main/java/com/SubToGgunobo/nationwar/listeners/PlayerListener.java
+++ src/main/java/com/SubToGgunobo/nationwar/listeners/PlayerListener.java
@@ -7,7 +7,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        // 플레이어 입장 시 처리 로직
+        // 플레이어 입장 시 환영 메시지 전송
+        event.getPlayer().sendMessage("환영합니다! 즐거운 게임 되세요!");
     }
 }
-
