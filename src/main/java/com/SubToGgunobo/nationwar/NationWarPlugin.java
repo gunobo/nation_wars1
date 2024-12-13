186da5895d830fe563cbcf61c05e30a06e9bdeb5
gunobo <113815207+gunobo@users.noreply.github.com>
2024-12-13 20:04:22 +0900

    Update NationWarPlugin.java

--- src/main/java/com/SubToGgunobo/nationwar/NationWarPlugin.java
+++ src/main/java/com/SubToGgunobo/nationwar/NationWarPlugin.java
@@ -1,24 +1,27 @@
 package com.yourname.nationwar;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import com.yourname.nationwar.listeners.*;
 
 public class NationWarPlugin extends JavaPlugin {
+    
     @Override
-public void onEnable() {
-    nationManager = new NationManager();
-    // 루트 디렉토리에서 nations.txt를 읽어오도록 경로 설정
-    nationManager.loadNationsFromFile(getDataFolder() + "/../nations.txt"); 
-    getLogger().info("국가전쟁 플러그인이 활성화되었습니다.");
-    getServer().getPluginManager().registerEvents(new BrewingStandListener(), this);
-    getServer().getPluginManager().registerEvents(new EnchantmentListener(), this);
-    getServer().getPluginManager().registerEvents(new PersonalChestListener(), this);
-    getServer().getPluginManager().registerEvents(new RTPListener(), this);
-    getServer().getPluginManager().registerEvents(new DeathBanListener(), this);
-    getServer().getPluginCommand("국가").setExecutor(new NationCommandListener(nationManager));
-}
+    public void onEnable() {
+        // 리스너 등록
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+        getServer().getPluginManager().registerEvents(new RTPListener(), this);
+        getServer().getPluginManager().registerEvents(new PersonalChestListener(), this);
+        getServer().getPluginManager().registerEvents(new BrewingStandListener(), this);
+        getServer().getPluginManager().registerEvents(new DeathBanListener(), this);
+        getServer().getPluginManager().registerEvents(new EnchantmentListener(), this);
+        getServer().getPluginManager().registerEvents(new NationCommandListener(new NationManager()), this); // NationManager는 필요에 따라 인스턴스화
+        
+        getLogger().info("NationWarPlugin이 활성화되었습니다!");
+    }
 
     @Override
     public void onDisable() {
-        getLogger().info("국가전쟁 플러그인이 비활성화되었습니다.");
+        // 플러그인 비활성화 시 처리 로직 (필요한 경우)
+        getLogger().info("NationWarPlugin이 비활성화되었습니다.");
     }
 }
